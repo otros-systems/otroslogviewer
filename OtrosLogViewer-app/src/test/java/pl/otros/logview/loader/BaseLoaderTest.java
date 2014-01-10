5ef69c370767cd9d3a75b09b55de6fd3db6a44a2
Krzysztof <otros.systems@gmail.com>
2014-01-10 21:29:09 +0100

    Tests - change way of defining dirs - now woks with IntellijJ and Jenkins

--- OtrosLogViewer-app/src/test/java/pl/otros/logview/loader/BaseLoaderTest.java
+++ OtrosLogViewer-app/src/test/java/pl/otros/logview/loader/BaseLoaderTest.java
@@ -20,6 +20,8 @@ import org.junit.Test;
 import pl.otros.logview.filter.LogFilter;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Collection;
 
@@ -28,22 +30,27 @@ public class BaseLoaderTest {
   private BaseLoader baseLoader = new BaseLoader();
 
   @Test
-  public void testLoad() {
-    // test/resources/plugins/filters/
-    Collection<LogFilter> load = baseLoader.load(new File("./src/test/resources/plugins/filters/"), LogFilter.class);
+  public void testLoad() throws URISyntaxException {
+    File dir = getFile("plugins/filters/");
+    Collection<LogFilter> load = baseLoader.load(dir, LogFilter.class);
     Assert.assertEquals(2, load.size());
   }
 
+
   @Test
-  public void testLoadFromDir() {
-    Collection<LogFilter> load = baseLoader.loadFromDir(new File("./src/test/resources/plugins/filters/"), LogFilter.class);
+  public void testLoadFromDir() throws URISyntaxException {
+    Collection<LogFilter> load = baseLoader.loadFromDir(getFile("plugins/filters/"), LogFilter.class);
     Assert.assertEquals(1, load.size());
   }
 
   @Test
-  public void testLoadFromJar() {
-    Collection<LogFilter> load = baseLoader.loadFromJar(new File("./src/test/resources/plugins/filters/filters.jar"), LogFilter.class);
+  public void testLoadFromJar() throws URISyntaxException {
+    Collection<LogFilter> load = baseLoader.loadFromJar(getFile("plugins/filters/filters.jar"), LogFilter.class);
     Assert.assertEquals(1, load.size());
   }
 
+  private File getFile(String name) throws URISyntaxException {
+    URI uri = this.getClass().getClassLoader().getResource(name).toURI();
+    return new File(uri);
+  }
 }
