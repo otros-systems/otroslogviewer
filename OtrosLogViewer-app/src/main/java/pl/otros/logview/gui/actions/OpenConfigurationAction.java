41188f7fce59507a5f2b940ff404b3a042b54491
Krzysztof <otros.systems@gmail.com>
2014-03-29 17:51:52 +0100

    Issue 54: Add configuration editor

--- OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/actions/OpenConfigurationAction.java
+++ OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/actions/OpenConfigurationAction.java
@@ -10,16 +10,17 @@ import pl.otros.swing.config.ConfigComponent;
 import pl.otros.swing.config.ConfigView;
 import pl.otros.swing.config.provider.ConfigurationProvider;
 import pl.otros.swing.config.provider.ConfigurationProviderImpl;
-import pl.otros.swing.config.test.DateFormatView;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 public class OpenConfigurationAction extends OtrosAction {
-
   public OpenConfigurationAction(OtrosApplication otrosApplication) {
     super("Preferences", Icons.GEAR, otrosApplication);
-    putValue(ACCELERATOR_KEY ,KeyStroke.getKeyStroke('p'));
+    putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F12, InputEvent.CTRL_MASK));
+    putValue(MNEMONIC_KEY, KeyEvent.VK_P);
   }
 
   @Override
@@ -35,9 +36,8 @@ public class OpenConfigurationAction extends OtrosAction {
         new LogDateFormatConfigView(),
         new IdeIntegrationConfigView(getOtrosApplication()),
         new VersionCheckConfigView()
-
-    }                                                      ;
-    ConfigComponent configComponent = new ConfigComponent(configurationProvider, actionAfterSave,null,configViews);
+    };
+    ConfigComponent configComponent = new ConfigComponent(configurationProvider, actionAfterSave, null, configViews);
     getOtrosApplication().addClosableTab("Preferences", "OtrosLogViewerPreferences", Icons.GEAR, configComponent, true);
   }
 }
