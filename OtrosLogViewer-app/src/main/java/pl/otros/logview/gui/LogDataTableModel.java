3ad40eeff318309e93d08f99d02050063d6d2f7c
kovacsz81@gmail.com <>
2014-09-26 13:30:21 +0200

    Replace tabs and new lines in log view table

--- OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/LogDataTableModel.java
+++ OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/LogDataTableModel.java
@@ -112,7 +112,7 @@ public class LogDataTableModel extends AbstractTableModel implements LogDataColl
         result = ld.getLevel();
         break;
       case MESSAGE:
-        result = ld.getMessage();
+        result = ld.getMessage().replace("\t", "    ").replace("\n", "|");
         break;
       case CLASS:
         String clazz = ld.getClazz();
