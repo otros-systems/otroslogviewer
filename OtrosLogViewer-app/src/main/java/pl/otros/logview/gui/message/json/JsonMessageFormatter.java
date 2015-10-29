e6e9ba63a1aabaa578aefb0a4162cb4d4631666b
Krzysztof <otros.systems@gmail.com>
2015-10-29 17:21:39 +0100

    FIx JSON formatter

--- OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/message/json/JsonMessageFormatter.java
+++ OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/message/json/JsonMessageFormatter.java
@@ -2,6 +2,8 @@ package pl.otros.logview.gui.message.json;
 
 import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.otros.logview.gui.message.MessageFormatter;
 import pl.otros.logview.gui.message.SubText;
 import pl.otros.logview.pluginable.AbstractPluginableElement;
@@ -10,46 +12,48 @@ import java.util.ArrayList;
 
 public class JsonMessageFormatter extends AbstractPluginableElement implements MessageFormatter {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(JsonMessageFormatter.class.getName());
+  public JsonMessageFormatter() {
+    super("Json formatter", "Formats json in message");
+  }
 
-    public JsonMessageFormatter() {
-        super("Json formatter", "Formats json in message");
+  public JsonFinder jsonFinder = new JsonFinder();
+
+  @Override
+  public int getApiVersion() {
+    return MESSAGE_FORMATTER_VERSION_1;
+  }
+
+  @Override
+  public boolean formattingNeeded(String message) {
+    return jsonFinder.findJsonFragments(message).size() > 0;
+  }
+
+  @Override
+  public String format(String message) {
+    final ArrayList<SubText> jsonFragments = jsonFinder.findJsonFragments(message);
+    StringBuilder sb = new StringBuilder();
+    int lastEnd = 0;
+    for (SubText jsonFragment : jsonFragments) {
+      sb.append(message.substring(lastEnd, jsonFragment.getStart()));
+      final String group = jsonFragment.subString(message);
+      String toAppend = group;
+      try {
+        JSONObject o = new JSONObject(group);
+        final String jsonFormatted = o.toString(2);
+        toAppend = jsonFormatted;
+      } catch (JSONException e) {
+        LOGGER.debug("There is no need to format {}",group);
+      }
+      if (!sb.toString().endsWith("\n")) {
+        sb.append("\n");
+      }
+      sb.append(toAppend).append("\n");
+      lastEnd = jsonFragment.getEnd();
     }
-
-    public JsonFinder jsonFinder = new JsonFinder();
-
-    @Override
-    public int getApiVersion() {
-        return MESSAGE_FORMATTER_VERSION_1;
-    }
-
-    @Override
-    public boolean formattingNeeded(String message) {
-        return jsonFinder.findJsonFragments(message).size() > 0;
-    }
-
-    @Override
-    public String format(String message) {
-        final ArrayList<SubText> jsonFragments = jsonFinder.findJsonFragments(message);
-        StringBuilder sb = new StringBuilder();
-        int lastEnd = 0;
-        for (SubText jsonFragment : jsonFragments) {
-
-            sb.append(message.substring(lastEnd, jsonFragment.getStart()));
-            final String group = jsonFragment.subString(message);
-            try {
-                JSONObject o = new JSONObject(group);
-                if (!sb.toString().endsWith("\n")){
-                    sb.append("\n");
-                }
-                sb.append(o.toString(2))
-                        .append("\n");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            lastEnd = jsonFragment.getEnd();
-        }
-        sb.append(message.substring(lastEnd));
-        return sb.toString().trim();
-    }
-
+    sb.append(message.substring(lastEnd));
+    return sb.toString().trim();
+  }
 }
+
+
