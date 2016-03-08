dd844d0f18036c25d6e16ddf8c0f72135b217093
Krzysztof <otros.systems@gmail.com>
2016-03-08 08:38:17 +0100

    Disable the confirmation message on exit #247

--- OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/actions/ExitAction.java
+++ OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/actions/ExitAction.java
@@ -15,6 +15,8 @@
  ******************************************************************************/
 package pl.otros.logview.gui.actions;
 
+import net.miginfocom.swing.MigLayout;
+import org.apache.commons.configuration.DataConfiguration;
 import pl.otros.logview.gui.ConfKeys;
 import pl.otros.logview.gui.OtrosApplication;
 
@@ -44,11 +46,16 @@ public class ExitAction extends OtrosAction implements WindowListener {
   }
 
   protected void askAndExit() {
-    boolean doConfirm = getOtrosApplication().getConfiguration().getBoolean(ConfKeys.CONFIRM_QUIT, true);
-    if (!doConfirm || JOptionPane.showConfirmDialog(
-      frame,
-      "Do you want to exit OtrosLogViewer and parse logs with 'grep'?",
-      "Are you sure?", JOptionPane.YES_NO_OPTION)
+    final DataConfiguration configuration = getOtrosApplication().getConfiguration();
+    boolean doConfirm = configuration.getBoolean(ConfKeys.CONFIRM_QUIT, true);
+    JPanel panel = new JPanel(new MigLayout("left"));
+    panel.add(new JLabel("Do you want to exit OtrosLogViewer and parse logs with 'grep'?"),"growx, wrap");
+    getOtrosApplication().getConfiguration().getBoolean(ConfKeys.CONFIRM_QUIT,true);
+    final JCheckBox box = new JCheckBox("Always ask before exit", doConfirm);
+    box.addActionListener(e -> configuration.setProperty(ConfKeys.CONFIRM_QUIT, box.isSelected()));
+    panel.add(box,"growx, wrap");
+
+    if (!doConfirm || JOptionPane.showConfirmDialog(frame, panel, "Are you sure?", JOptionPane.YES_NO_OPTION)
       == JOptionPane.YES_OPTION) {
       frame.setVisible(false);
       frame.dispose();
