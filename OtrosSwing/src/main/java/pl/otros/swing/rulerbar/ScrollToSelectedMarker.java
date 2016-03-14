952fa987c42f8472c1b4aba149edaa280b4decc6
Krzysztof <otros.systems@gmail.com>
2016-03-15 00:25:53 +0100

    Fix codacy issues

--- OtrosSwing/src/main/java/pl/otros/swing/rulerbar/ScrollToSelectedMarker.java
+++ OtrosSwing/src/main/java/pl/otros/swing/rulerbar/ScrollToSelectedMarker.java
@@ -3,14 +3,9 @@ package pl.otros.swing.rulerbar;
 
 public class ScrollToSelectedMarker implements MarkerClickListener {
 
-
-  public ScrollToSelectedMarker() {
-  }
-
   @Override
   public void markerClicked(Marker marker) {
     marker.markerClicked();
-
   }
 
 }
