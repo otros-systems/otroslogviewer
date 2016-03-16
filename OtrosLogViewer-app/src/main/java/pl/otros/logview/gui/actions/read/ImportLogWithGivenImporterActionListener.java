b52df0dfe3180b456c767781123c245a1f1d6c24
Krzysztof <otros.systems@gmail.com>
2016-03-16 08:50:38 +0100

    Fix codacy issues - remove unused code

--- OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/actions/read/ImportLogWithGivenImporterActionListener.java
+++ OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/actions/read/ImportLogWithGivenImporterActionListener.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011 Krzysztof Otrebski
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,15 +18,12 @@ package pl.otros.logview.gui.actions.read;
 import pl.otros.logview.gui.OtrosApplication;
 import pl.otros.logview.importer.LogImporter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class ImportLogWithGivenImporterActionListener extends ImportLogActionListener {
 
   private LogImporter importer;
 
   public ImportLogWithGivenImporterActionListener(OtrosApplication otrosApplication, LogImporter importer) {
-    super(otrosApplication,"Open " + importer.getName() + " log");
+    super(otrosApplication, "Open " + importer.getName() + " log");
     this.importer = importer;
   }
 
