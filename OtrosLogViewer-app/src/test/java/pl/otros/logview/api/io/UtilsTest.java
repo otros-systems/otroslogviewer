a3cfaa6b819b294a02f8f1e67f219928fdadf0cf
krzysztof <krzysztof.otrebski@gmail.com>
2016-12-08 18:49:28 +0100

    423 - detect log event start in stream

--- OtrosLogViewer-app/src/test/java/pl/otros/logview/api/io/UtilsTest.java
+++ OtrosLogViewer-app/src/test/java/pl/otros/logview/api/io/UtilsTest.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright 2011 Krzysztof Otrebski
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
 package pl.otros.logview.api.io;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
@@ -59,10 +44,10 @@ import static org.mockito.Mockito.when;
 
 public class UtilsTest {
 
-  public static final int PORT = 45501;
+  private static final int PORT = 45501;
   private static final String HTTP_GZIPPED = "http://127.0.0.1:" + PORT + "/log.txt.gz";
   private static final String HTTP_NOT_GZIPPED = "http://127.0.0.1:" + PORT + "/log.txt";
-  public static FileSystemManager fsManager;
+  private static FileSystemManager fsManager;
   private static WireMockServer wireMock;
 
   @BeforeClass
@@ -158,9 +143,8 @@ public class UtilsTest {
 
   @Test
   public void testLoadHttpNotGzippedBufferedReader() throws Exception {
-    String url = HTTP_NOT_GZIPPED;
     LoadingInfo loadingInfo = Utils.openFileObject(fsManager
-        .resolveFile(url));
+        .resolveFile(HTTP_NOT_GZIPPED));
     InputStream contentInputStream = loadingInfo.getContentInputStream();
     // byte[] expectedBytes =
     // IOUtils.toByteArray(fsManager.resolveFile(url).getContent().getInputStream());
@@ -390,7 +374,7 @@ public class UtilsTest {
 
   private static class LogImporterStartingWithT extends DetectOnTheFlyLogImporter {
 
-    public LogImporterStartingWithT() {
+    LogImporterStartingWithT() {
       super(Collections.emptyList());
     }
 
