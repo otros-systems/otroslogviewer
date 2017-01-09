dc187f775f5ce374b4b77cfd09355ed9bb16b7a9
krzysztof <krzysztof.otrebski@gmail.com>
2017-01-09 23:16:29 +0100

    432 Serializers in persist service

--- OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/suggestion/PersistedSuggestionSource.java
+++ OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/suggestion/PersistedSuggestionSource.java
@@ -1,8 +1,5 @@
 package pl.otros.logview.gui.suggestion;
 
-import org.apache.sling.commons.json.JSONArray;
-import org.apache.sling.commons.json.JSONException;
-import org.apache.sling.commons.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.otros.logview.api.services.Deserializer;
@@ -13,17 +10,13 @@ import pl.otros.swing.suggest.SuggestionQuery;
 import pl.otros.swing.suggest.SuggestionSource;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class PersistedSuggestionSource implements SuggestionSource<SearchSuggestion> {
   private static final Logger LOGGER = LoggerFactory.getLogger(PersistedSuggestionSource.class.getName());
 
   private static final String KEY = "searchHistory";
-  private static final String SEARCH_MODE = "searchMode";
-  private static final String QUERY = "query";
+
   private PersistService persistService;
   private SearchSuggestionSource decorate;
   private final Serializer<List<SearchHistory>, String> serlizer;
@@ -32,37 +25,9 @@ public class PersistedSuggestionSource implements SuggestionSource<SearchSuggest
   public PersistedSuggestionSource(SearchSuggestionSource decorate, PersistService persistService) {
     this.persistService = persistService;
     this.decorate = decorate;
-    serlizer = value -> {
-      final List<JSONObject> collect = value.stream().map(s -> {
-        final HashMap<String, String> map = new HashMap<>();
-        map.put(SEARCH_MODE, s.getSearchMode().name());
-        map.put(QUERY, s.getQuery());
-        return new JSONObject(map);
-      }).collect(Collectors.toList());
-      final JSONArray jsonArray = new JSONArray(collect);
-      return jsonArray.toString();
-    };
-    deserialiser = data -> {
-      try {
-        JSONArray a = new JSONArray(data);
-        final int length = a.length();
-        final ArrayList<SearchHistory> searchHistories = new ArrayList<>(length);
-        for (int i = 0; i < length; i++) {
-          try {
-            final JSONObject o = a.getJSONObject(i);
-            final String searchMde = o.getString(SEARCH_MODE);
-            final String query = o.getString(QUERY);
-            searchHistories.add(new SearchHistory(SearchAction.SearchMode.valueOf(searchMde), query));
-          } catch (Exception e) {
-            LOGGER.warn("Cant parse JSON object, ignoring it", e);
-          }
-        }
-        return Optional.of(searchHistories);
-      } catch (JSONException e) {
-        LOGGER.error("Can't parse data: " + data, e);
-      }
-      return Optional.empty();
-    };
+    final SearchHistorySerialization searchHistorySerialization = new SearchHistorySerialization();
+    serlizer = searchHistorySerialization.serializer();
+    deserialiser = searchHistorySerialization.deserializer();
     decorate.setHistory(load());
   }
 
