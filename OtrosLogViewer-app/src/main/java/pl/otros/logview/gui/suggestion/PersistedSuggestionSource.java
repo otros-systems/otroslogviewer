278887b146058a6e97a322a15f6fd754c3accdf2
krzysztof <krzysztof.otrebski@gmail.com>
2017-01-09 20:35:30 +0100

    432 Serializers in persist service

--- OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/suggestion/PersistedSuggestionSource.java
+++ OtrosLogViewer-app/src/main/java/pl/otros/logview/gui/suggestion/PersistedSuggestionSource.java
@@ -21,7 +21,9 @@ import java.util.stream.Collectors;
 public class PersistedSuggestionSource implements SuggestionSource<SearchSuggestion> {
   private static final Logger LOGGER = LoggerFactory.getLogger(PersistedSuggestionSource.class.getName());
 
-  public static final String KEY = "searchHistory";
+  private static final String KEY = "searchHistory";
+  private static final String SEARCH_MODE = "searchMode";
+  private static final String QUERY = "query";
   private PersistService persistService;
   private SearchSuggestionSource decorate;
   private final Serializer<List<SearchHistory>, String> serlizer;
@@ -31,38 +33,33 @@ public class PersistedSuggestionSource implements SuggestionSource<SearchSuggest
     this.persistService = persistService;
     this.decorate = decorate;
     serlizer = value -> {
-      //TODO serialize to JSON
       final List<JSONObject> collect = value.stream().map(s -> {
         final HashMap<String, String> map = new HashMap<>();
-        map.put("searchMode", s.getSearchMode().name());
-        map.put("query", s.getQuery());
+        map.put(SEARCH_MODE, s.getSearchMode().name());
+        map.put(QUERY, s.getQuery());
         return new JSONObject(map);
       }).collect(Collectors.toList());
       final JSONArray jsonArray = new JSONArray(collect);
       return jsonArray.toString();
     };
     deserialiser = data -> {
-      //TODO deserialize json
       try {
         JSONArray a = new JSONArray(data);
         final int length = a.length();
-        //TODO change to IntSream.map
         final ArrayList<SearchHistory> searchHistories = new ArrayList<>(length);
         for (int i = 0; i < length; i++) {
           try {
             final JSONObject o = a.getJSONObject(i);
-            final String searchMde = o.getString("searchMode");
-            final String query = o.getString("query");
-            final SearchHistory searchHistory = new SearchHistory(SearchAction.SearchMode.valueOf(searchMde), query);
-            searchHistories.add(searchHistory);
+            final String searchMde = o.getString(SEARCH_MODE);
+            final String query = o.getString(QUERY);
+            searchHistories.add(new SearchHistory(SearchAction.SearchMode.valueOf(searchMde), query));
           } catch (Exception e) {
             LOGGER.warn("Cant parse JSON object, ignoring it", e);
           }
         }
         return Optional.of(searchHistories);
       } catch (JSONException e) {
-        //TODO handle errors
-        e.printStackTrace();
+        LOGGER.error("Can't parse data: " + data, e);
       }
       return Optional.empty();
     };
@@ -76,11 +73,6 @@ public class PersistedSuggestionSource implements SuggestionSource<SearchSuggest
       history.remove(item);
     }
     history.add(0, item);
-    decorate.setHistory(history);
-    persist(history);
-  }
-
-  protected void setHistory(List<SearchHistory> history) {
     decorate.setHistory(history);
     persist(history);
   }
