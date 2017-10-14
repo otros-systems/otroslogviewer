af48167681b1a256fa89da3cb40f996191519e15
krzysztof <krzysztof.otrebski@gmail.com>
2017-10-14 08:34:43 +0200

    Issue #448 NullPointer when browsing files

--- OtrosVfsBrowser/src/main/java/pl/otros/vfs/browser/table/FileNameWithTypeTableCellRenderer.java
+++ OtrosVfsBrowser/src/main/java/pl/otros/vfs/browser/table/FileNameWithTypeTableCellRenderer.java
@@ -18,6 +18,8 @@ package pl.otros.vfs.browser.table;
 
 import org.apache.commons.vfs2.FileName;
 import org.apache.commons.vfs2.FileType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.otros.vfs.browser.Icons;
 import pl.otros.vfs.browser.ParentFileObject;
 import pl.otros.vfs.browser.util.VFSUtils;
@@ -29,6 +31,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FileNameWithTypeTableCellRenderer extends DefaultTableCellRenderer {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(FileNameWithTypeTableCellRenderer.class.getName());
 
   private Set<String> archivesSuffixes;
 
@@ -48,31 +52,38 @@ public class FileNameWithTypeTableCellRenderer extends DefaultTableCellRenderer 
   public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
 
     JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-    FileNameWithType fileNameWithType = (FileNameWithType) value;
-    FileName fileName = fileNameWithType.getFileName();
-    label.setText(fileName.getBaseName());
-    label.setName("VfsBrowser.file." + fileName.getBaseName());
-    label.setToolTipText(fileName.getFriendlyURI());
+    if (value != null) {
+      FileNameWithType fileNameWithType = (FileNameWithType) value;
+      FileName fileName = fileNameWithType.getFileName();
+      label.setText(fileName.getBaseName());
+      label.setName("VfsBrowser.file." + fileName.getBaseName());
+      label.setToolTipText(fileName.getFriendlyURI());
 
-    FileType fileType = fileNameWithType.getFileType();
-    Icon icon = null;
-    Icons icons = Icons.getInstance();
-    if (fileNameWithType.getFileName().getBaseName().equals(ParentFileObject.PARENT_NAME)) {
-      icon = icons.getArrowTurn90();
-    } else if (FileType.FOLDER.equals(fileType)) {
-      icon = icons.getFolderOpen();
-    } else if (VFSUtils.isArchive(fileName)) {
-      if ("jar".equalsIgnoreCase(fileName.getExtension())) {
-        icon = icons.getJarIcon();
-      } else {
-        icon = icons.getFolderZipper();
+      FileType fileType = fileNameWithType.getFileType();
+      Icon icon = null;
+      Icons icons = Icons.getInstance();
+      if (fileNameWithType.getFileName().getBaseName().equals(ParentFileObject.PARENT_NAME)) {
+        icon = icons.getArrowTurn90();
+      } else if (FileType.FOLDER.equals(fileType)) {
+        icon = icons.getFolderOpen();
+      } else if (VFSUtils.isArchive(fileName)) {
+        if ("jar".equalsIgnoreCase(fileName.getExtension())) {
+          icon = icons.getJarIcon();
+        } else {
+          icon = icons.getFolderZipper();
+        }
+      } else if (FileType.FILE.equals(fileType)) {
+        icon = icons.getFile();
+      } else if (FileType.IMAGINARY.equals(fileType)) {
+        icon = icons.getShortCut();
       }
-    } else if (FileType.FILE.equals(fileType)) {
-      icon = icons.getFile();
-    } else if (FileType.IMAGINARY.equals(fileType)) {
-      icon = icons.getShortCut();
+      label.setIcon(icon);
+    } else {
+      LOGGER.warn("Rendering for null value: {},{}", row, column);
+      label.setText("");
+      label.setIcon(null);
     }
-    label.setIcon(icon);
+
     return label;
   }
 
