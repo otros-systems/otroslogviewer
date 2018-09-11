a93bd6be768740c59b680f7c5c9813b341d87ae6
Krzysztof <krzysztof.otrebski@gmail.com>
2018-09-11 20:46:24 +0200

    #543 Fix NPE when "show selected thread" (#545)

--- OtrosLogViewer-app/src/main/java/pl/otros/logview/accept/SelectedThreadAcceptCondition.java
+++ OtrosLogViewer-app/src/main/java/pl/otros/logview/accept/SelectedThreadAcceptCondition.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright 2011 Krzysztof Otrebski
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
 package pl.otros.logview.accept;
 
 import com.google.common.base.Joiner;
@@ -20,6 +5,7 @@ import pl.otros.logview.api.gui.LogDataTableModel;
 import pl.otros.logview.api.model.LogData;
 
 import javax.swing.*;
+import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -49,7 +35,7 @@ public class SelectedThreadAcceptCondition extends SelectionAwareAcceptCondition
     int[] selectedRows = jTable.getSelectedRows();
     for (int i : selectedRows) {
       LogData logData = dataTableModel.getLogData(jTable.convertRowIndexToModel(i));
-      threads.add(logData.getThread());
+      threads.add(Optional.ofNullable(logData.getThread()).orElse("--"));
     }
 
     description = threads.size() + " threads: " + Joiner.on(", ").join(threads);
