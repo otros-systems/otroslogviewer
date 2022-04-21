fc62b4185c3254950b7dca9074ae2d3cad74abe7
Christian Nüssgens <cnuessgens@conet.de>
2022-04-21 15:35:16 +0200

    #618 respect numbers in filenames for sorting in open-file-dialog

--- olv-vfs/src/main/java/pl/otros/vfs/browser/table/FileNameWithTypeComparator.java
+++ olv-vfs/src/main/java/pl/otros/vfs/browser/table/FileNameWithTypeComparator.java
@@ -16,11 +16,15 @@
 
 package pl.otros.vfs.browser.table;
 
-import pl.otros.vfs.browser.ParentFileObject;
+import java.math.BigInteger;
+import java.util.Comparator;
+import java.util.regex.Pattern;
+
+import javax.swing.SortOrder;
+
 import org.apache.commons.vfs2.FileType;
 
-import javax.swing.*;
-import java.util.Comparator;
+import pl.otros.vfs.browser.ParentFileObject;
 
 public class FileNameWithTypeComparator implements Comparator<FileNameWithType> {
   private SortOrder sortOrder = SortOrder.ASCENDING;
@@ -31,35 +35,90 @@ public class FileNameWithTypeComparator implements Comparator<FileNameWithType> 
   }
 
   public int compareTo(FileNameWithType o1, FileNameWithType o2) {
-    if (o1 == null || o1.getFileType() == null || o1.getFileName() == null) {
-      return -1;
-    }
-    if (o2 == null || o2.getFileType() == null || o2.getFileName() == null) {
-      return 1;
-    }
-    //folders first first
-    boolean folder1 = FileType.FOLDER.equals(o1.getFileType());
-    boolean folder2 = FileType.FOLDER.equals(o2.getFileType());
-    int result = 0;
-
-    int sortOrderSign = SortOrder.ASCENDING.equals(sortOrder) ? 1 : -1;
-    if (o1.getFileName().getBaseName().equalsIgnoreCase(ParentFileObject.PARENT_NAME)) {
-      result = -1 * sortOrderSign;
-    } else if (o2.getFileName().getBaseName().equalsIgnoreCase(ParentFileObject.PARENT_NAME)) {
-      result = 1 * sortOrderSign;
-    } else if (folder1 & !folder2) {
-      result = -1 * sortOrderSign;
-    } else if (!folder1 & folder2) {
-      result = 1 * sortOrderSign;
-    } else {
-      result = o1.getFileName().compareTo(o2.getFileName());
-    }
-
-    return result;
+    Comparator<FileNameWithType> comp = sortOrder == SortOrder.ASCENDING ? COMP : COMP_REVERSED;
+    return comp.compare(o1, o2);
   }
 
 
   public void setSortOrder(SortOrder sortOrder) {
     this.sortOrder = sortOrder;
   }
+
+  private static final Comparator<FileNameWithType> NULL_FIRST = (o1, o2) -> {
+    if (o1 == null || o1.getFileType() == null || o1.getFileName() == null) {
+      return -1;
+    } else if (o2 == null || o2.getFileType() == null || o2.getFileName() == null) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+
+  private static final Comparator<FileNameWithType> PARENT_FIRST = (o1, o2) -> {
+    if (o1.getFileName().getBaseName().equalsIgnoreCase(ParentFileObject.PARENT_NAME)) {
+      return -1;
+    } else if (o2.getFileName().getBaseName().equalsIgnoreCase(ParentFileObject.PARENT_NAME)) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+
+  private static final Comparator<FileNameWithType> FOLDER_FIRST = (o1, o2) -> {
+    // folders first first
+    boolean folder1 = FileType.FOLDER.equals(o1.getFileType());
+    boolean folder2 = FileType.FOLDER.equals(o2.getFileType());
+    if (folder1 & !folder2) {
+      return -1;
+    } else if (!folder1 & folder2) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+
+  private static final Comparator<FileNameWithType> BY_NAME = (o1, o2) -> {
+    return o1.getFileName().compareTo(o2.getFileName());
+  };
+
+  private static final Pattern NUMBERS = Pattern.compile("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+  private static final Comparator<FileNameWithType> BY_NAME_RESPECTING_NUMBERS = (o1, o2) -> {
+    String s1 = o1.getFileName().getURI();
+    String s2 = o2.getFileName().getURI();
+    // see https://stackoverflow.com/a/48946875
+    // Splitting both input strings by NUMBERS patterns
+    String[] split1 = NUMBERS.split(s1);
+    String[] split2 = NUMBERS.split(s2);
+    for (int i = 0; i < Math.min(split1.length, split2.length); i++) {
+      char c1 = split1[i].charAt(0);
+      char c2 = split2[i].charAt(0);
+      int cmp = 0;
+
+      // If both segments start with a digit, sort them numerically using
+      // BigInteger to stay safe
+      if (c1 >= '0' && c1 <= '9' && c2 >= 0 && c2 <= '9') {
+        cmp = new BigInteger(split1[i]).compareTo(new BigInteger(split2[i]));
+      }
+
+      // If we haven't sorted numerically before, or if numeric sorting yielded
+      // equality (e.g 007 and 7) then sort lexicographically
+      if (cmp == 0) {
+        cmp = split1[i].compareTo(split2[i]);
+      }
+
+      // Abort once some prefix has unequal ordering
+      if (cmp != 0) {
+        return cmp;
+      }
+    }
+
+    // If we reach this, then both strings have equally ordered prefixes, but
+    // maybe one string is longer than the other (i.e. has more segments)
+    return split1.length - split2.length;
+  };
+
+  private static final Comparator<FileNameWithType> STABLE = NULL_FIRST.thenComparing(PARENT_FIRST).thenComparing(FOLDER_FIRST);
+  private static final Comparator<FileNameWithType> COMP = STABLE.thenComparing(BY_NAME_RESPECTING_NUMBERS);
+  private static final Comparator<FileNameWithType> COMP_REVERSED = STABLE.reversed().thenComparing(BY_NAME_RESPECTING_NUMBERS);
+
 }
