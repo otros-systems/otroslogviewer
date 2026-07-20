d4f1825cec84fd0e295726d4459c66c953fa2a52
Sven Nissel <sven.nissel@hexagon.com>
2026-07-20 10:19:28 +0200

    Fix typo

--- olv-dist/src/test/java/pl/otros/logview/dist/FileNewLineEncodingTest.java
+++ olv-dist/src/test/java/pl/otros/logview/dist/FileNewLineEncodingTest.java
@@ -11,8 +11,8 @@ import static org.testng.AssertJUnit.assertFalse;
 public class FileNewLineEncodingTest {
   @Test
   public void testShellScriptUnixNewLineEncoding() throws IOException {
-    assertFalse("The file olv.sh contains widows new line ('\\r'). Remove it or you cannot use this script with linux!", readFile("olv.sh").contains("\r"));
-    assertFalse("The file olv-batch.sh contains widows new line ('\\r'). Remove it or you cannot use this script with linux!", readFile("olv-batch.sh").contains("\r"));
+    assertFalse("The file olv.sh contains windows new line ('\\r'). Remove it or you cannot use this script with linux!", readFile("olv.sh").contains("\r"));
+    assertFalse("The file olv-batch.sh contains windows new line ('\\r'). Remove it or you cannot use this script with linux!", readFile("olv-batch.sh").contains("\r"));
   }
 
   private String readFile(String file) throws IOException {
