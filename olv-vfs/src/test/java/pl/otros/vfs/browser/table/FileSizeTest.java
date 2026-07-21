8fea517edc14e46dd1c93c3dc38642ae0fccaddb
Sven Nissel <sven.nissel@hexagon.com>
2026-07-21 22:26:36 +0200

    Fix Testng execution

--- olv-vfs/src/test/java/pl/otros/vfs/browser/table/FileSizeTest.java
+++ olv-vfs/src/test/java/pl/otros/vfs/browser/table/FileSizeTest.java
@@ -16,105 +16,106 @@
 
 package pl.otros.vfs.browser.table;
 
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
- * Created with IntelliJ IDEA.
- * User: krzyh
- * Date: 10/30/13
- * Time: 7:43 AM
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: krzyh Date: 10/30/13 Time: 7:43 AM To change this template use File | Settings | File Templates.
  */
 public class FileSizeTest {
+    private final static char DECIMAL_SEPARATOR = DecimalFormatSymbols.getInstance(Locale.getDefault()).getDecimalSeparator();
 
-  @DataProvider (name = "testFileSizeDataProvider")
-  public Object[][] testFileSizeDataProvider() {
-    return new Object[][]{
-        {"0", 0l},
-        {"0 B", 0l},
-        {"0B", 0l},
-        {"1kB", 1024l},
-        {"1 kB", 1024l},
-        {"1,5kB", 1024l + 512},
-        {"1.5kB", 1024l + 512},
-        {"1 MB", 1024l * 1024},
-        {"1 GB", 1024l * 1024 * 1024},
-        {"1 TB", 1024l * 1024 * 1024 * 1024}
-    };
-  }
+    @DataProvider(name = "testFileSizeDataProvider")
+    public Object[][] testFileSizeDataProvider() {
+        return new Object[][] {
+            {"0", 0l},
+            {"0 B", 0l},
+            {"0B", 0l},
+            {"1kB", 1024l},
+            {"1 kB", 1024l},
+            {"1,5kB", 1024l + 512},
+            {"1.5kB", 1024l + 512},
+            {"1 MB", 1024l * 1024},
+            {"1 GB", 1024l * 1024 * 1024},
+            {"1 TB", 1024l * 1024 * 1024 * 1024}
+        };
+    }
 
-  @Test (dataProvider = "testFileSizeDataProvider")
-  public void testFileSize(String constructorArgs, long bytes) {
-    Assert.assertEquals(new FileSize(constructorArgs).getBytes(), bytes);
-  }
+    @Test(dataProvider = "testFileSizeDataProvider")
+    public void testFileSize(String constructorArgs, long bytes) {
+        Assert.assertEquals(new FileSize(constructorArgs).getBytes(), bytes);
+    }
 
+    @DataProvider(name = "testToStringDataProvider")
+    Object[][] testToStringDataProvider() {
+        return new Object[][] {
+            {0L, "0 B"},
+            {100L, "100 B"},
+            {10L * 1024, "10 KB"},
+            {15L * 1024 * 1024, "15 MB"},
+            {25L * 1024 * 1024 * 1024, "25 GB"},
+            {35L * 1024 * 1024 * 1024 * 1024, "35 TB"},
+            {1536L, "1" + DECIMAL_SEPARATOR + "5 KB"},
+            {1025L, "1 KB"},
+            {1024L * 1024 - 1, "1024 KB"}
 
-  @DataProvider (name = "testToStringDataProvider")
-  Object[][] testToStringDataProvider() {
-    return new Object[][]{
-        {0, "0 B"},
-        {100, "100 B"},
-        {10 * 1024, "10 KB"},
-        {15l * 1024 * 1024, "15 MB"},
-        {25l * 1024 * 1024 * 1024, "25 GB"},
-        {35l * 1024 * 1024 * 1024 * 1024, "35 TB"},
-        {1536, "1.5 KB"},
-        {1025, "1 KB"},
-        {1024 * 1024 - 1, "1024 KB"}
+        };
+    }
 
-    };
-  }
+    @Test
+    public void testToString_withNull() {
+        System.err.println("");
+    }
 
-  @Test (dataProvider = "testToStringDataProvider")
-  public void testToString(long size, String expectedResult) throws Exception {
-    FileSize fileSize = new FileSize(size);
-    Assert.assertEquals(fileSize.toString(), expectedResult);
-  }
+    @Test(dataProvider = "testToStringDataProvider")
+    public void testToString(long size, String expectedResult) throws Exception {
+        FileSize fileSize = new FileSize(size);
+        Assert.assertEquals(fileSize.toString(), expectedResult);
+    }
 
-  @DataProvider (name = "testGetMultiplier")
-  Object[][] testGetMultiplierDataProvider() {
-    return new Object[][]{
-        {'T', 1024l * 1024 * 1024 * 1024},
-        {'t', 1024l * 1024 * 1024 * 1024},
-        {'g', 1024 * 1024 * 1024},
-        {'G', 1024 * 1024 * 1024},
-        {'m', 1024 * 1024},
-        {'M', 1024 * 1024},
-        {'K', 1024},
-        {'k', 1024},
-        {'?', 1}
-    };
+    @DataProvider(name = "testGetMultiplier")
+    Object[][] testGetMultiplierDataProvider() {
+        return new Object[][] {
+            {'T', 1024L * 1024 * 1024 * 1024},
+            {'t', 1024L * 1024 * 1024 * 1024},
+            {'g', 1024L * 1024 * 1024},
+            {'G', 1024L * 1024 * 1024},
+            {'m', 1024L * 1024},
+            {'M', 1024L * 1024},
+            {'K', 1024L},
+            {'k', 1024L},
+            {'?', 1L}
+        };
 
-  }
+    }
 
-  @Test (dataProvider = "testGetMultiplier")
-  public void testGetMultiplier(char c, long multiplier) throws Exception {
-    FileSize fs = new FileSize(1);
-    Assert.assertEquals(fs.getMultiplier(c), multiplier);
-  }
+    @Test(dataProvider = "testGetMultiplier")
+    public void testGetMultiplier(char c, long multiplier) throws Exception {
+        FileSize fs = new FileSize(1);
+        Assert.assertEquals(fs.getMultiplier(c), multiplier);
+    }
 
+    @Test(dataProvider = "testToStringDataProvider")
+    public void testConvertToStringRepresentation(long size, String expectedResult) throws Exception {
+        Assert.assertEquals(FileSize.convertToStringRepresentation(size), expectedResult);
+    }
 
-  @Test (dataProvider = "testToStringDataProvider")
-  public void testConvertToStringRepresentation(long size, String expectedResult) throws Exception {
-    Assert.assertEquals(FileSize.convertToStringRepresentation(size), expectedResult);
-  }
+    @DataProvider(name = "testCompareTo")
+    public Object[][] testCompareToDataProvider() {
+        return new Object[][] {
+            {1L, 0L, 1},
+            {Long.MAX_VALUE, Long.MIN_VALUE, 1},
+            {Long.MAX_VALUE, Long.MAX_VALUE, 0},
+            {Long.MIN_VALUE, Long.MIN_VALUE, 0},
+            {0L, 0L, 0},
+        };
+    }
 
-
-  @DataProvider (name = "testCompareTo")
-  public Object[][] testCompareToDataProvider() {
-    return new Object[][]{
-        {1, 0, 1},
-        {Long.MAX_VALUE, Long.MIN_VALUE, 1},
-        {Long.MAX_VALUE, Long.MAX_VALUE, 0},
-        {Long.MIN_VALUE, Long.MIN_VALUE, 0},
-        {0, 0, 0},
-    };
-  }
-
-  @Test (dataProvider = "testCompareTo")
-  public void testCompareTo(long size1, long size2, int result) throws Exception {
-    Assert.assertEquals(new FileSize(size1).compareTo(new FileSize(size2)), result);
-  }
+    @Test(dataProvider = "testCompareTo")
+    public void testCompareTo(long size1, long size2, int result) throws Exception {
+        Assert.assertEquals(new FileSize(size1).compareTo(new FileSize(size2)), result);
+    }
 }
--- olv-vfs/src/test/java/pl/otros/vfs/browser/util/URIUtilsTest.java
+++ olv-vfs/src/test/java/pl/otros/vfs/browser/util/URIUtilsTest.java
@@ -1,52 +1,45 @@
 package pl.otros.vfs.browser.util;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class URIUtilsTest {
-  private URIUtils classUnderTest;
+    private final URIUtils classUnderTest = new URIUtils();
 
-  @Before
-  public void setUp() {
-    classUnderTest = new URIUtils();
-  }
+    @Test
+    public void testGetFriendlyURI_handlesNullURI() {
+        Assert.assertNull(classUnderTest.getFriendlyURI(null), "Expected null when URI parameter is null");
+    }
 
-  @Test
-  public void testGetFriendlyURI_handlesNullURI() {
-    Assert.assertNull("Expected null when URI parameter is null", classUnderTest.getFriendlyURI(null));
-  }
+    @Test
+    public void testGetFriendlyURI_handlesURINoCredentials() {
+        String uri = "sftp://hostname:21/path/file.log";
+        String actual = classUnderTest.getFriendlyURI(uri);
+        Assert.assertEquals(actual, uri, "Expected the same uri");
+    }
 
-  @Test
-  public void testGetFriendlyURI_handlesURINoCredentials() {
-    String uri = "sftp://hostname:21/path/file.log";
-    String actual = classUnderTest.getFriendlyURI(uri);
-    Assert.assertEquals("Expected the same uri", uri, actual);
-  }
+    @Test
+    public void testGetFriendlyURI_handlesURIWithCredentials() {
+        String uri = "sftp://username:password@hostname:21/path/file.log";
+        String expected = "sftp://username:***@hostname:21/path/file.log";
+        String actual = classUnderTest.getFriendlyURI(uri);
+        Assert.assertEquals(actual, expected, "Expected masked password in URI");
+        Assert.assertTrue(actual.contains(":***@"), "Password should be replaced with 3 asterisks");
+    }
 
-  @Test
-  public void testGetFriendlyURI_handlesURIWithCredentials() {
-    String uri = "sftp://username:password@hostname:21/path/file.log";
-    String expected = "sftp://username:***@hostname:21/path/file.log";
-    String actual = classUnderTest.getFriendlyURI(uri);
-    Assert.assertEquals("Expected masked password in URI", expected, actual);
-    Assert.assertTrue("Password should be replaced with 3 asterisks", actual.contains(":***@"));
-  }
+    @Test
+    public void testGetFriendlyURI_handlesURIWithUsernameNoPassword() {
+        String uri = "sftp://username@hostname:21/path/file.log";
+        String actual = classUnderTest.getFriendlyURI(uri);
+        Assert.assertEquals(actual, uri, "Expected masked password in URI");
+    }
 
-
-  @Test
-  public void testGetFriendlyURI_handlesURIWithUsernameNoPassword() {
-    String uri = "sftp://username@hostname:21/path/file.log";
-    String actual = classUnderTest.getFriendlyURI(uri);
-    Assert.assertEquals("Expected masked password in URI", uri, actual);
-  }
-
-  @Test
-  public void testGetFriendlyURI_handlesMalformedURI() {
-    String uri = "sftp:username:password@hostname:21/path/file.log";
-    String expected = "sftp:***@hostname:21/path/file.log";
-    String actual = classUnderTest.getFriendlyURI(uri);
-    Assert.assertEquals("Expected 'sftp' to be recognized as the username, and the actual credentials to be masked", expected, actual);
-  }
+    @Test
+    public void testGetFriendlyURI_handlesMalformedURI() {
+        String uri = "sftp:username:password@hostname:21/path/file.log";
+        String expected = "sftp:***@hostname:21/path/file.log";
+        String actual = classUnderTest.getFriendlyURI(uri);
+        Assert.assertEquals(actual, expected, "Expected 'sftp' to be recognized as the username, and the actual credentials to be masked");
+    }
 
 }
